f83057f08e014e2b26082685c31de1b14f0b5a6b
郑杰 <411426414@qq.com>
2020-08-21 21:49:54 +0800

    first commit

---
+++ src/views/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <main-header
+      title='Function Music'
+      @nav-click='toPath'
+    >
+    </main-header>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MainHeader from '@/components/Main/MainHeader'
+
+export default {
+  methods: {
+    toPath(path) {
+      this.$router.push(path);
+    }
+  },
+  components: {
+    MainHeader
+  }
+}
+</script>
